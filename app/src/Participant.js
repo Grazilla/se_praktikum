3916a7137f664c10af66e4b6059a0466e1e78b39
unknown <sara.scheucher@gmail.com>
2020-01-30 12:26:20 +0100

    QR Code gelöscht

--- app/src/Participant.js
+++ app/src/Participant.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
+import QrReader from 'react-qr-reader';
 
 
 class Participant extends Component {
@@ -15,27 +16,47 @@ class Participant extends Component {
   async componentDidMount() {
     const response = await fetch('/api/race');
     const body = await response.json();
-    this.setState({races: body, isLoading: false, userName:"", passwort:""});
+    this.setState({ races: body, isLoading: false, userName: "", passwort: "" });
+  }
+
+  handleScan = data => {
+    if (data) {
+      this.setState({
+        result: data
+      })
+    }
+  }
+  handleError = err => {
+    console.error(err)
   }
 
   render() {
-    const {races, isLoading} = this.state;
+    const { races, isLoading } = this.state;
 
-    if(isLoading) {
+    if (isLoading) {
       return <p>Loading...</p>;
     }
 
     console.log(this.state.selectedRace);
 
     return (
-      
+
       <div className="App">
         <header className="App-header">
           <div className="App-intro">
-            <h2>Hello Player</h2>       
-          
+            <h2>Hello Player</h2>
+            <div>
+              <QrReader
+                delay={300}
+                onError={this.handleError}
+                onScan={this.handleScan}
+                style={{ width: '100%' }}
+              />
+              <p>{this.state.result}</p>
+            </div>
+
           </div>
-          <Button  href="/" size="lg">Back</Button> 
+          <Button href="/" size="lg">Back</Button>
         </header>
 
       </div>
