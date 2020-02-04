3d3790f729d2eb64295a69a913a887853a6c8b43
Deathacus <34517761+Deathacus@users.noreply.github.com>
2020-02-05 00:38:37 +0100

    added new viewer view

--- app/src/Home.js
+++ app/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import Rois from "./Rois.jpeg";
 
 function Home() {
   return (
@@ -8,14 +9,17 @@ function Home() {
         <div className="col">&nbsp;</div>
         <div className="col-md-10 col-lg-8 col-sm-12">
           <h5>Welcome to GPS Race</h5>
-          <h6>the most exciting race at JKU (aside from the Pondrace...)</h6>
+          <h6>The most exciting race at JKU (aside from the Pondrace...)</h6>
           <p>
             Implemented as part of the SE practicum by:
           </p>
           <ul>
-            <li>Someone</li>
-            <li>Someone else</li>
-            <li>Us</li>
+            <li>Lukas Grüneis</li>
+            <li>Lisa Kammerer</li>
+            <li>Sandra Pühringer</li>
+            <li>Sara Scheucher</li>
+            <p></p>
+            <img src={Rois} alt="bild" />
           </ul>
         </div>
         <div className="col">&nbsp;</div>
--- app/src/Viewer.js
+++ app/src/Viewer.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Map, Marker, Popup, TileLayer, Polyline, Circle } from 'react-leaflet';
 import './Viewer.css';
+import Accordion from 'react-bootstrap/Accordion';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 class Viewer extends React.Component {
   constructor(props) {
     super(props);
-    const { match: { params } } = this.props;    
+    const { match: { params } } = this.props;
     this.state = { raceId: params.raceId, participants: [], mapPosition: [0, 0], route: [[0, 0]], matchpoints: [] };
   }
 
@@ -14,11 +17,11 @@ class Viewer extends React.Component {
     fetch(url)
       .then(response => response.json())
       .then(body => {
-          this.setState({
-            participants: body.participants
-          });
-          console.log(body);
+        this.setState({
+          participants: body.participants
         });
+        console.log(body.participants);
+      });
   }
 
   componentDidMount() {
@@ -27,8 +30,8 @@ class Viewer extends React.Component {
       .then(response => response.json())
       .then(body => {
         let coords = [];
-        for(let matchpoint of body.matchpoints) {
-           coords.push([matchpoint.latitude, matchpoint.longitude]);
+        for (let matchpoint of body.matchpoints) {
+          coords.push([matchpoint.latitude, matchpoint.longitude]);
         }
         this.setState({
           route: coords,
@@ -36,6 +39,7 @@ class Viewer extends React.Component {
           participants: body.participants,
           mapPosition: [body.mapLatitude, body.mapLongitude]
         });
+
       });
 
     this.interval = setInterval(() => this.getStandings(), 1000);
@@ -47,35 +51,67 @@ class Viewer extends React.Component {
 
   render() {
     return (
-      <div className="container" style={{marginTop: 20 +'px'}}>
-        <div className="row">
-          <div className="card col-md-4 col-sm-12">
-            <div className="card-body">
-              <h5 className="card-title">Current Standings</h5>
-              {this.state.participants && this.state.participants.map((value, index) => {
-                //always use ids as key if possible when the ordering inside the list changes for efficiency reasons (less DOM mutation)
-                return <div key={'card' + value.participant.id}><b>{value.participant.name}</b> Waypoints: {value.nextMatchpoint ? value.nextMatchpoint.oid : '-'}, Distance: {value.distance} m</div>
-              })}
+      <div style={{ backgroundColor: "#b3dfe5" }}>
+        <div className="container" style={{ marginTop: 20 + 'px' }}>
+          <div className="row">
+            <div className="card col-md-4 col-sm-12">
+              <div className="card-body">
+                <h5 className="card-title">Current Standings</h5>
+                <div className="card mb-3">
+                  <div className="row no-gutters">
+                    <div className="col-md-1">
+                      {
+                        this.state.participants.map((value, index) => {
+                          return <div key={'card' + value.id}>{index + 1}.
+                            <div className="col-md-4">
+                              <img src="https://steamuserimages-a.akamaihd.net/ugc/885378750233363168/CFA6808BAC06EC8983BE6D9E13D2D8790FAD3307/" width="100" height="100" alt="Yoshi"></img>
+                            </div>
+                            <div className="col-md-7">
+                              <div className="card-body">
+                                <Accordion defaultActiveKey="0">
+                                  <Card>
+                                    <Card.Header>
+                                      <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                                        {value.participant.name}
+                                      </Accordion.Toggle>
+                                    </Card.Header>
+                                    <Accordion.Collapse eventKey="0">
+                                      <Card.Body>
+                                        Next matchpoint: {value.nextMatchpoint.oid}, Distance: {value.distance}m
+                                      </Card.Body>
+                                    </Accordion.Collapse>
+                                  </Card>
+                                </Accordion>
+                              </div>
+                            </div>
+                          </div>
+                        })
+                      }
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-
-          <div className="col-md-1">&nbsp;</div>
-          <div className="card col-md-7 col-sm-12">
-            <div className="card-body">
-              <h5 className="card-title">Map View</h5>
-              <Map center={this.state.mapPosition} zoom={17}>
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                />
-                {this.state.participants && this.state.participants.map((value, index) => {
-                  return <Marker key={'marker' + value.participant.id} position={[value.latitude, value.longitude]}><Popup>{value.participant.name}</Popup></Marker>
-                })}
-                <Polyline color="lime" positions={this.state.route} />
-                {this.state.matchpoints && this.state.matchpoints.map((value, index) => {
-                  return <Circle key={'matchpoint' + value.id} center={[value.latitude, value.longitude]} color="purple" />
-                })}
-              </Map>
+            <div className="col-md-1">&nbsp;</div>
+            <div className="card col-md-7 col-sm-12">
+              <div className="card-body">
+                <h5 className="card-title">Map View</h5>
+                <div className="leaflet-container">
+                  <Map center={this.state.mapPosition} zoom={17}>
+                    <TileLayer
+                      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                      attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+                    />
+                    {this.state.participants.map((value, index) => {
+                      return <Marker key={'marker' + value.id} position={[value.latitude, value.longitude]}><Popup>{value.username}</Popup></Marker>
+                    })}
+                    <Polyline color="lime" positions={this.state.route} />
+                    {this.state.route.map((value, index) => {
+                      return <Circle key={'waypoint' + index} center={[value[0], value[1]]} color="purple" />
+                    })}
+                  </Map>
+                </div>
+              </div>
             </div>
           </div>
         </div>
